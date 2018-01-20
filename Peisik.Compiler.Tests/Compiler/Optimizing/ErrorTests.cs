34be37ea3bff53900ff138b26d176e7780a31598
Petri Laarne <polsys@users.noreply.github.com>
2018-01-20 12:03:38 +0200

    Handle local assignments

--- Peisik.Compiler.Tests/Compiler/Optimizing/ErrorTests.cs
+++ Peisik.Compiler.Tests/Compiler/Optimizing/ErrorTests.cs
@@ -8,6 +8,22 @@ namespace Polsys.Peisik.Tests.Compiler.Optimizing
     partial class ErrorTests : CompilerTestBase
     {
         [Test]
+        public void AssignmentToConst()
+        {
+            var source = @"
+private int Value 4
+
+public void Main()
+begin
+  Value = 4
+end";
+            (var _, var diagnostics) = CompileOptimizedWithDiagnostics(source, Optimization.None);
+
+            Assert.That(diagnostics, Has.Exactly(1).Items);
+            Assert.That(diagnostics[0].Diagnostic, Is.EqualTo(DiagnosticCode.MayNotAssignToConst));
+        }
+
+        [Test]
         public void LocalAlreadyDefined()
         {
             var source = @"
@@ -21,6 +37,24 @@ end";
 
             Assert.That(diagnostics, Has.Exactly(1).Items);
             Assert.That(diagnostics[0].Diagnostic, Is.EqualTo(DiagnosticCode.NameAlreadyDefined));
+        }
+
+        [Test]
+        public void MismatchingTypesInAssignment()
+        {
+            var source = @"
+public int Main()
+begin
+  int local 5
+  local = true
+  return local
+end";
+            (var _, var diagnostics) = CompileOptimizedWithDiagnostics(source, Optimization.None);
+
+            Assert.That(diagnostics, Has.Exactly(1).Items);
+            Assert.That(diagnostics[0].Diagnostic, Is.EqualTo(DiagnosticCode.WrongType));
+            Assert.That(diagnostics[0].AssociatedToken, Is.EqualTo("Bool"));
+            Assert.That(diagnostics[0].Expected, Is.EqualTo("Int"));
         }
 
         [Test]
@@ -72,6 +106,21 @@ end";
             Assert.That(diagnostics[0].Diagnostic, Is.EqualTo(DiagnosticCode.WrongType));
             Assert.That(diagnostics[0].AssociatedToken, Is.EqualTo("Bool"));
             Assert.That(diagnostics[0].Expected, Is.EqualTo("Int"));
+        }
+
+        [Test]
+        public void NameNotFound_Local()
+        {
+            var source = @"
+public void Main()
+begin
+  LOCAL = 5
+end";
+            (var _, var diagnostics) = CompileOptimizedWithDiagnostics(source, Optimization.None);
+
+            Assert.That(diagnostics, Has.Exactly(1).Items);
+            Assert.That(diagnostics[0].Diagnostic, Is.EqualTo(DiagnosticCode.NameNotFound));
+            Assert.That(diagnostics[0].AssociatedToken, Is.EqualTo("LOCAL"));
         }
     }
 }
